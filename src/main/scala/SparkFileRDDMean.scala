3a55ae82e9c4e11ad263bb6e9aa2f0ece438924e
kiran.learn7@gmail.com <kiran.learn7@gmail.com>
2018-03-19 20:28:27 -0500

    Commiting

--- src/main/scala/SparkFileRDDMean.scala
+++ src/main/scala/SparkFileRDDMean.scala
@@ -20,6 +20,7 @@ object SparkFileRDDMean {
 
     val sc = new SparkContext(conf)
 
+    //Option# 1
     var linesRDD = sc.textFile(filepath).map(x=>x.substring(1,x.size-1).split(';')).filter(x => x(0) != "age").
       map(x => ( x(0).toInt, x(1).substring(2,x(1).size-2), x(2).substring(2, x(2).size-2),
         x(3).substring(2, x(3).size-2), x(4).substring(2, x(4).size-2), x(5).toFloat,
@@ -29,6 +30,8 @@ object SparkFileRDDMean {
         x(15).substring(2, x(15).size-2), x(16).substring(2, x(16).size-2) ))
 
     linesRDD.take(15).foreach(println)
+
+    //Option#2
 
     //Get the mean/average of y="yes"
     val dMean = linesRDD.map(x => (1, (if(x._17=="yes") 1 else 0, 1))).
--- src/main/scala/SparkStreamKafka010TopicDemo.scala
+++ src/main/scala/SparkStreamKafka010TopicDemo.scala
@@ -3,10 +3,12 @@
   * Description:
   *   The program gives a report of total sale quantity per a time period & product
   * Input/Source data format: (txn_no,txn_dt,mem_num,store_id,sku_id,qty,paid): 1000000003,2017-06-01 08:00:00,1345671,S876,4685,2,3.98
-  * Output/Report: txn_dt, sku_id, qty_total
+  * Output/Report: Prints all messages from all partitions
+  *   txn_dt, sku_id, qty_total
   *   2017-06-01 08:00:00,3245,2
   *   2017-06-01 08:00:00,4685,7
   *
+  *   Messages by each partition.
   * Steps:
   * 1. Start zookeeper service
   * 2. Start kafka broker(server) at port 9092 ($KAFHA_HOME/config/server.properties)
@@ -25,26 +27,30 @@ import org.apache.spark.sql.{Row, SQLContext, SparkSession}
 import org.apache.spark.streaming.dstream.InputDStream
 import kafka.serializer.StringDecoder
 import org.apache.kafka.common.serialization.StringDeserializer
+import org.apache.spark.streaming.kafka010.ConsumerStrategies.Subscribe
+import org.apache.spark.streaming.kafka010.LocationStrategies.PreferConsistent
 //import _root_.kafka.serializer.StringDecoder
 import org.apache.spark.{SparkConf, SparkContext}
 import org.apache.spark.streaming.{Seconds, StreamingContext, Milliseconds}
-//import org.apache.spark.streaming.kafka.KafkaUtils //*** Uncomment for old version
+//import org.apache.spark.streaming.kafka.KafkaUtils
+//import org.apache.spark.streaming.kafka._
 import org.apache.spark.storage.StorageLevel
 import org.apache.spark._
 import org.apache.spark.internal.Logging
 import org.apache.hadoop.fs._
 import org.apache.spark.sql.hive.HiveContext
-
 import org.apache.kafka.common.serialization.StringDeserializer
+import org.apache.spark.streaming.kafka010.KafkaUtils
+import org.apache.spark.streaming.kafka010.{HasOffsetRanges, CanCommitOffsets, OffsetRange}
 
-object SparkStreamKafkaTopicDemo {
+object SparkStreamKafka010TopicDemo {
 
   def main(args: Array[String]) {
 
-  //Using SparkSession & SparkContext
+    //Using SparkSession & SparkContext
     val spark = SparkSession
       .builder()
-      .master("local[*]")
+      .master("local[20]")
       .appName("Spark Streaming data from Kafka")
       .getOrCreate()
 
@@ -55,52 +61,46 @@ object SparkStreamKafkaTopicDemo {
     //val ssc = new StreamingContext(spark.sparkContext.getConf, Seconds(5))
     //Above gives Error: Only one SparkContext may be running in this JVM (see SPARK-2243). To ignore this error, set spark.driver.allowMultipleContexts = true
 
-    val ssc = new StreamingContext(spark.sparkContext, Seconds(30))
+    val ssc = new StreamingContext(spark.sparkContext, Seconds(10))
 
     //StreamingContext without SparkContext
     //val ssc = new StreamingContext("local[2]", "Spark Streaming data from Kafka", Seconds(30))
 
-    ssc.checkpoint("_checkpointing")
-    /*//Context using sparkConf *WORKS*
-    val sparkConf = new SparkConf()
-      .setAppName("Spark Streaming data from Kafka")
-      .setMaster("local[*]")
-      .set("spark.executor.memory", "1g")
-    val ssc = new StreamingContext(sparkConf, Seconds(10))
-    //This may give error on console that already SparkContext is running & can't create another context, -then
-    //Stop sc (for above error): sc.stop()
-    */
+    //ssc.checkpoint("_checkpointing")
 
-    val kafkaParams: Map[String, String] = Map(
+    val kafkaParams: Map[String, Object] = Map(
       //"zookeeper.connect" -> "localhost:2181",
       "bootstrap.servers" -> "localhost:9092"   //org.apache.spark.SparkException: Must specify metadata.broker.list or bootstrap.servers
       //"metadata.broker.list" -> "localhost:9092",
-      ,"group.id" -> "kafkaStreamData"
-      //,"key.deserializer" -> classOf[org.apache.kafka.common.serialization.StringDeserializer]
-      //,"value.deserializer" -> classOf[org.apache.kafka.common.serialization.StringDeserializer]
-      //,"key.deserializer.class" -> "kafka.serializer.StringEncoder"
+      ,"group.id" -> "kafkaStreamDataxx3"
+      ,"key.deserializer" -> classOf[org.apache.kafka.common.serialization.StringDeserializer]
+      ,"value.deserializer" -> classOf[org.apache.kafka.common.serialization.StringDeserializer]
       ,"zookeeper.session.timeout.ms" ->	"500"
       ,"zookeeper.sync.time.ms" -> "250"
       ,"auto.commit.interval.ms" ->	"1000"
-      ,"auto.offset.reset" -> "largest" //"smallest"/"largest": smallest pulls all messages from beginning
+      ,"auto.offset.reset" -> "earliest" //latest, earliest, none
       ,"spark.streaming.kafka.maxRatePerPartition" -> ""
-      //,"enable.auto.commit" -> false
+      ,"enable.auto.commit" -> (false: java.lang.Boolean)
     )
-    val topicsSet: Set[String] = "txnstopic".split(",").map(_.trim).toSet
-    //val topicsArray = "txnstopic".split(",")
 
-/*
-    val messages: InputDStream[(String, String)] = KafkaUtils.createDirectStream[String, String, StringDecoder, StringDecoder](
-      ssc, kafkaParams, topicsSet)
-
-
-    //Total rows & data in this stream
-    messages.count().print()
-    messages.print()
+    val topics = Array("kafkatopic2") //This topic has PartitionCount:10
 
     //process each RDD
-    messages.foreachRDD( rdd =>  {
-      val prodSalesTotal = rdd.map(_._2)
+    val stream = KafkaUtils.createDirectStream[String, String](
+      ssc,
+      PreferConsistent,
+      Subscribe[String, String](topics, kafkaParams)
+    )
+    stream.foreachRDD(
+      rdd =>  {
+        val offsetRanges = rdd.asInstanceOf[HasOffsetRanges].offsetRanges
+        //val o: OffsetRange = offsetRanges(TaskContext.get.partitionId)
+        //println(s"${o.topic} ${o.partition} ${o.fromOffset} ${o.untilOffset}")
+
+        rdd.foreach(println)
+
+
+      val prodSalesTotal = rdd.map(rec => (rec.key, rec.value)).map(_._2)
         .filter(rec => rec.split(",").length == 7)
         //filter lines if qty>0
         .filter(rec => rec.split(",")(5).toInt > 0)
@@ -112,29 +112,17 @@ object SparkStreamKafkaTopicDemo {
 
       //print("Partitions count: "+x.partitions.length)
       println("Total rows count: " + rdd.count())
-      rdd.take(15).foreach(println)
+      //rdd.take(15).foreach(println)
       println("Result rows count:" + prodSalesTotal.count())
-      prodSalesTotal.take(15).foreach(println)
+      prodSalesTotal.take(50).foreach(println)
 
-      /*
-      // Below function call will save the data into HDFS
-      if(prodSalesTotal.count()>0)
-        //prodSalesTotal.coalesce(1).saveAsTextFile("/home/kiran/km_hadoop_fs/warehouse/prod_qty_" + getDT4FileName)
-        prodSalesTotal.toDF().write.mode(SaveMode.Append).save("/home/kiran/km_hadoop_fs/warehouse/prod_qty_" + getDT4FileName)
-      */
+        //see messages count by partition
+       rdd.map(rec => (rec.partition, 1)).reduceByKey(_ + _).foreach(println)
+        stream.asInstanceOf[CanCommitOffsets].commitAsync(offsetRanges)
     })
-*/
+
     ssc.start()
     ssc.awaitTermination()
   }
 
-  def getDT4FileName(): String ={
-
-    val dateFormatter = new SimpleDateFormat("yyyy_MM_dd_hh_mm_ss")
-    return dateFormatter.format(new Date())
-
-    //val today = Calendar.getInstance.getTime
-    //return dateFormatter.format(today)
-
-  }
 }

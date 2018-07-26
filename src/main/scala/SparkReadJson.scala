8c233f15ad24fa35ef8666be6fc319966b02a98b
kiran5july <kiran.learn7@gmail.com>
2018-07-26 13:17:11 -0500

    Update SparkReadJson.scala

--- src/main/scala/SparkReadJson.scala
+++ src/main/scala/SparkReadJson.scala
@@ -1,104 +1,67 @@
-import org.apache.spark.{SparkConf, SparkContext}
-import org.apache.spark.sql.{SparkSession,SQLContext}
-import org.apache.spark.sql.functions
-import org.apache.spark.sql.functions._
-import org.apache.spark.sql.types._
-import org.apache.spark.sql.Column
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.functions.when
+import org.apache.spark.sql.types.{IntegerType, StringType, StructField, StructType}
 
-/***** WORKING ****
-  * Created by kiran on 2/8/17.
+/**
+  * 2 options:
+  * 1) Read directly as JSON
+  * 2) Read as text & extract each element
   */
 object SparkReadJson {
 
   def main(args: Array[String]) {
 
-    //Using sparkSession
     val spark = SparkSession
       .builder()
+      .config("spark.sql.warehouse.dir", "file:///c:/tmp/spark-warehouse")
+      .config("spark.executor.memory", "2g")
+      .config("spark.yarn.executor.memoryOverhead", "10g")
       .master("local")
-      .appName("Spark Read delimited file")
-      //     .config("spark.sql.warehouse.dir", warehouseLocation)
-      //     .enableHiveSupport()
-      .getOrCreate()
-    //set new runtime options
-    spark.conf.set("spark.sql.shuffle.partitions", 6)
-    spark.conf.set("spark.executor.memory", "2g")
-val sqlContext = spark.sqlContext
+      .appName("Spark DF")
+      .getOrCreate
 
-    val lines1 = spark.read.json("/home/kiran/km/km_big_data/data/data_nested_struct_col.json")
-    //Sample: {"id":1,"nested_col": {"key1": "1.1", "key2": ["1.21", "1.22"], "key3": {"key3_1": "1.31", "key3_2": "1.32"}}}
-    //nested_col is struct with 3 columns (key1 is string, key2 is Array() and key3 is struct)
-    lines1.show(false)
-    lines1.printSchema()
+    //spark.conf.set("spark.executor.memory", "2g")
 
-    //This withColumn("a.b.c") is considering as a new column, but selects the correct column
-    //lines2.withColumn("a.b.c", lines2("a.b.c")).show()
+    val sc = spark.sparkContext
+    sc.setLogLevel("ERROR")
 
-    println("After changing column type...")
-    val nestedCol = lines1.withColumn("id", lines1("id").cast(IntegerType)).//Can use cast("int")
-      withColumn("nested_col", struct(lines1("nested_col.key1").cast(FloatType).as("key1"),
-                                       lines1("nested_col.key2"),
-                                        struct(lines1("nested_col.key3.key3_1").cast(FloatType).as("key3_1"),
-                                               lines1("nested_col.key3.key3_2"))
-                                          .as("key3")))
+    val sJSONPath = "file:///C://km//avro//sample_avro_data_as_json.json"
+    //Sample data:
+    //{"performCheck" : "N", "clientTag" :{"key":"111"}, "contactPoint": {"email":"abc@gmail.com", "type":"EML"}}
+    //{"performCheck" : "N", "clientTag" :{"key":"222"}, "contactPoint": {"email":"def@gmail.com", "type":"EML"}}
+    //{"performCheck" : "Y", "clientTag" :{"key":"333"}, "contactPoint": {"email":"ccc.com", "type":"EML"}}
 
-    nestedCol.printSchema()
-    //nestedCol.show(20)
+    import spark.implicits._
 
-    val lines2 = spark.read.json("/home/kiran/km/km_hadoop/data/data_nested_struct_col2.json")
-    //data: {"a": {"b": {"c": "1.31", "d": "1.11"}}, "TimeStamp": "2017-02-18", "id":1}
-    lines2.show(false)
-    lines2.printSchema()
+//DIRECTLY READ AS JSON
+/*    val ds = spark.read.json(sJSONPath)
+    ds.show(false)
 
-    /*val df2 = lines2.withColumn("a", struct(
-                                        struct(
-                                            lines2("a.b.c").cast(DoubleType).as("c"),
-                                            lines2("a.b.d").as("d")
-                                        ).as("b")))
-                .withColumn("TimeStamp", lines2("TimeStamp").cast(DateType))
+    val ds2 = ds.withColumn("key", ds.col("clientTag.key"))
+      .withColumn("contact_type", ds.col("contactPoint")("type"))
+      .withColumn("contact_email", ds.col("contactPoint.email"))
+      //.drop("contactPoint")
+      //.drop("clientTag")
+      .withColumn("contactPoint.valid", when(ds.col("contactPoint.email").contains("@") === true, "Y").otherwise("N"))
+
+    ds2.show(false)
 */
 
-  //We can't reference child column directly to update **EXPLORE**
-    val df2 = lines2.withColumn("'a'.'b'.'c'",
-        lines2("'a'.'b'.'c'").cast(DoubleType).as("c")
-      )
 
-    df2.printSchema()
-    //df2.show(false)
+//READ AS TEXT FILE & PROCESS AS JSON
+    import org.apache.spark.sql.types._                         // include the Spark Types to define our schema
+    import org.apache.spark.sql.functions._                     // include the Spark helper functions
 
 
-    //df2.createOrReplaceTempView("test")
-    //val df3 = sqlContext.sql("describe test")
-    //df3.show(false)
+    val fileData = spark.sparkContext.textFile(sJSONPath)
+      .toDF("file_data")
 
-    //Flatten structure
-    val lines3 = spark.read.json("/home/kiran/km/km_hadoop/data/data_nested_struct_col3.json")
-    lines3.printSchema()
+    fileData.show(false)
 
-    //val df3 = lines3.select(flattenSchema(lines3.schema):_*)
-    //val df3 = lines3.withColumn("field2_2", (col("field2")).toString())
-      //.drop("field2")
-    //df3.printSchema()
-    //df3.show()
+    fileData.select(get_json_object($"file_data", "$.clientTag.key").alias("key"),
+                    get_json_object($"file_data", "$.contactPoint.email").alias("email"),
+                    get_json_object($"file_data", "$.performCheck").alias("performCheck")
+    ).withColumn("contactPoint.valid", when(col("email").contains("@") === true, "Y").otherwise("N"))
+      .show(false)
   }
-  def flattenSchema(schema: StructType, prefix: String = null) : Array[Column] = {
-    schema.fields.flatMap(f => {
-      val colName = if (prefix == null) f.name else (prefix + "." + f.name)
-
-      f.dataType match {
-        case st: StructType => flattenSchema(st, colName)
-        //case a: ArrayType => {
-          //a.productIterator.map(s2 => flattenSchema(s2.asInstanceOf[StructType].in, colName))
-          /*
-          var itr = a.productIterator
-          while(itr.hasNext)
-            flattenSchema(itr.next().asInstanceOf[StructType], colName))
-*/
-//Array(a.productIterator.to[StructField])
-        //}
-        case _ => Array(col(colName))
-      }
-    })
-  }
-
 }

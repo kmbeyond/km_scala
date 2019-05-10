a082c3f7e9ff443ba10f13cc07e283e8ee149c51
kiran5july <kiran.learn7@gmail.com>
2019-05-10 17:00:42 -0400

    Update json_csProcess01.scala

--- src/main/scala/json_csProcess01.scala
+++ src/main/scala/json_csProcess01.scala
@@ -13,7 +13,7 @@ object json_csProcess01 {
     //logger.info(getDT() + "; Class name=" + this.getClass.getName )
 
     //try {
-      //input arguments
+    //input arguments
     if (args.size < 2) {
       println("Please verify input arguments:");
       println("0: Database name (Ex: dev_data_comm)")
@@ -26,45 +26,25 @@ object json_csProcess01 {
     val inputHDFSPath = args(1);
     println(getDTFormat() + ": ***** arg(1): " + inputHDFSPath);
 
-      //val conf = new SparkConf()
-        //.setMaster("yarn") //yarn
-        //.setAppName("CS")
+    //val conf = new SparkConf()
+    //.setMaster("yarn") //yarn
+    //.setAppName("KM CS")
 
-      val spark = SparkSession.builder
-        .appName("CS")
-        .master("yarn")
-        .enableHiveSupport()
-        .getOrCreate()
+    val spark = SparkSession.builder
+      .appName("Commerce Signals")
+      .master("yarn")
+      .enableHiveSupport()
+      .getOrCreate()
     //.config("spark.sql.warehouse.dir", "/user/hive/warehouse")
     //.config("spark.hadoop.validateOutputSpecs", "false")
     //.config("spark.debug.maxToStringFields", 100)
-     spark.sparkContext.setLogLevel("ERROR")
+    spark.sparkContext.setLogLevel("ERROR")
 
-      import org.apache.spark.sql.functions._
-      //import spark.implicits._
-      import sys.process._
+    import org.apache.spark.sql.functions._
+    //import spark.implicits._
+    import sys.process._
 
-/*
-    import org.apache.spark.sql.{Row, SQLContext, SaveMode}
-    import org.apache.spark.sql.functions.{col, to_date, trim, udf, unix_timestamp, _}
-    //import org.apache.spark.sql.types.{StringType, StructField, StructType}
-    import org.apache.spark.{SparkConf, SparkContext}
-
-    val conf = new SparkConf().setAppName("KM Test")
-    val sc = new SparkContext(conf)
-    //val sqlContext = new SQLContext(sc)
-    //import sqlContext.implicits._
-    val hc = new org.apache.spark.sql.hive.HiveContext(sc)
-    //hc.sql("set spark.sql.caseSensitive=false")
-    //hc.sql("set hive.execution.engine=spark")
-    val spark = hc
-    //spark.tables("vivid").show()
-    //spark.tables().show()
-*/
     import spark.implicits._
-
-    //spark.sql("show databases").show(false)
-    //spark.conf.getAll.foreach(println)
 
     val proc_st_ts = new java.text.SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS").format(new java.util.Date())
 
@@ -106,7 +86,7 @@ object json_csProcess01 {
       System.exit(102)
     }
     println(getDTFormat() + s": --> ${sDBName}.cs_request")
-    df_cs_request.repartition(1).write.insertInto(sDBName+".cs_request")
+    df_cs_request.coalesce(1).write.insertInto(sDBName+".cs_request")
     //nodf_cs_request.createOrReplaceTempView("df_cs_request")
     run_time_ts = new java.text.SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS").format(new java.util.Date())
     //nospark.sql(s"insert into $sDBName.cs_request select request_id,'$proc_st_ts','$run_time_ts',description,start_date,end_date,time_zone,request_date,transaction_amount,transaction_count,matched_ids_count,transactor_count,stddev_spend_txn,stddev_spend_transactor,stddev_txncnt_transactor FROM df_cs_request")
@@ -123,7 +103,7 @@ object json_csProcess01 {
       System.exit(102)
     }
     println(getDTFormat() + s": --> ${sDBName}.cs_request_merchant")
-    df_cs_request_merchant.repartition(1).write.insertInto(sDBName+".cs_request_merchant")
+    df_cs_request_merchant.coalesce(1).write.insertInto(sDBName+".cs_request_merchant")
     //nodf_cs_request_merchant.createOrReplaceTempView("df_cs_request_merchant")
     run_time_ts = new java.text.SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS").format(new java.util.Date())
     //nospark.sql(s"INSERT into $sDBName.cs_request_merchant SELECT request_id,'$proc_st_ts','$run_time_ts',merchants,merchant_id FROM df_cs_request_merchant")
@@ -147,7 +127,7 @@ object json_csProcess01 {
     }
     val df_cs_request_audience = df_cs_request_audience_exp.select($"request_id", lit(proc_st_ts).as("process_ts"),lit(new java.text.SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS").format(new java.util.Date())).as("create_ts"), $"audience_id", $"uri", $"source", $"count")
     println(getDTFormat() + s": --> ${sDBName}.cs_request_audience")
-    df_cs_request_audience.repartition(1).write.insertInto(sDBName+".cs_request_audience")
+    df_cs_request_audience.coalesce(1).write.insertInto(sDBName+".cs_request_audience")
     run_time_ts = new java.text.SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS").format(new java.util.Date())
     //nodf_cs_request_audience.createOrReplaceTempView("df_cs_request_audience")
     //nospark.sql(s"insert into $sDBName.cs_request_audience select request_id,'$proc_st_ts','$run_time_ts',audiences FROM df_cs_request_audience")
@@ -156,28 +136,31 @@ object json_csProcess01 {
 
     //-------------004: cs_audience--------------
     /*
-    val req_aud_km = df_cs_request_audience.map(rec => (rec.getString(4), (rec.getString(0), rec.getString(3)))).rdd.groupByKey.mapValues(_.toList).collectAsMap
-    for ((fileName, fileInfo) <- req_aud_km)
-    {
-      println(getDTFormat() + ":--- Audience File: " + fileName);
-      val df_audience_file = spark.read.csv(inputHDFSPath + "/incoming/" + fileName)
-      if(df_audience_file.columns.size > 0 ) {
-        val df_audience = df_audience_file.toDF("cs_sha").select(lit(fileInfo(0)._1).as("request_id")
-          , lit(proc_st_ts).as("process_ts")
-          , lit(new java.text.SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS").format(new java.util.Date())).as("create_ts")
-          , lit(fileInfo(0)._2).as("audience_id"), $"cs_sha", lit("").as("impression_window")
-          //,lit(java.time.LocalDate.now.toString).as("process_date")
-        );
+        val req_aud_km = df_cs_request_audience.map(rec => (rec.getString(4), (rec.getString(0), rec.getString(3)))).rdd.groupByKey.mapValues(_.toList).collectAsMap
+        for ((fileName, fileInfo) <- req_aud_km)
+        {
+          println(getDTFormat() + ":--- Audience File: " + fileName);
+          val df_audience_file = spark.read.csv(inputHDFSPath + "/incoming/" + fileName)
 
-        //nodf_audience.repartition(1).write.insertInto(sDBName+".cs_audience")
-        df_audience.createOrReplaceTempView("df_audience");
-        println(getDTFormat() + s": --> ${sDBName}.cs_audience")
-        spark.sql(s"INSERT into $sDBName.cs_audience partition (process_date='" + (java.time.LocalDate.now) + s"') SELECT * from df_audience");
-        run_time_ts = new java.text.SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS").format(new java.util.Date())
-        println(getDTFormat() + s": Inserted ${fileName} in ${sDBName}.cs_audience");
-        //spark.sql(s"INSERT INTO TABLE $sDBName.cs_logs SELECT '$run_time_ts','cs','002_04','cs_audience',0,'Inserted Audience data file: $fileName; RowCount:" + df_audience.count + "'")
-      }
-    }*/
+    val df_audience=Seq(("","","","","","")).toDF("cs_sha","request_id","process_ts","create_ts","audience_id","impression_window")
+          if(df_audience_file.columns.size > 0 ) {
+            val df_audience = df_audience_file.toDF("cs_sha").select(lit(fileInfo(0)._1).as("request_id")
+              , lit(proc_st_ts).as("process_ts")
+              , lit(new java.text.SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS").format(new java.util.Date())).as("create_ts")
+              , lit(fileInfo(0)._2).as("audience_id"), $"cs_sha", lit("").as("impression_window")
+              //,lit(java.time.LocalDate.now.toString).as("process_date")
+            );
+
+            //nodf_audience.repartition(1).write.insertInto(sDBName+".cs_audience")
+            df_audience.createOrReplaceTempView("df_audience");
+            println(getDTFormat() + s": --> ${sDBName}.cs_audience")
+            spark.sql(s"INSERT into $sDBName.cs_audience partition (process_date='" + (java.time.LocalDate.now) + s"') SELECT * from df_audience");
+            run_time_ts = new java.text.SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS").format(new java.util.Date())
+            println(getDTFormat() + s": Inserted ${fileName} in ${sDBName}.cs_audience");
+            //spark.sql(s"INSERT INTO TABLE $sDBName.cs_logs SELECT '$run_time_ts','cs','002_04','cs_audience',0,'Inserted Audience data file: $fileName; RowCount:" + df_audience.count + "'")
+          }
+        }
+    */
     def funFileName: ((String) => String) = { (s) =>(s.split("/").last)}
     import org.apache.spark.sql.functions.udf
     val udfFileName = udf(funFileName)
@@ -197,7 +180,30 @@ object json_csProcess01 {
     println(getDTFormat() + s": Inserted in ${sDBName}.cs_audience");
 
     run_time_ts = new java.text.SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS").format(new java.util.Date())
-    spark.sql(s"INSERT INTO TABLE $sDBName.cs_logs SELECT '$run_time_ts','cs','002_04','cs_audience',0,'Inserted Audience datas'")
+    spark.sql(s"INSERT INTO TABLE $sDBName.cs_logs SELECT '$run_time_ts','cs','002_04','cs_audience',0,'Inserted Audience data'")
+
+    //--- Create Aud summary file - START
+    val df_audience_files=df_cs_request_audience.select("uri","request_id","audience_id").
+      join(df_audience_data, Seq("uri"), "left_outer").
+      select($"request_id",$"cs_sha", $"uri"  )
+
+    import sys.process._
+    var sCurrDt = new java.text.SimpleDateFormat("yyyy_MM_dd_HH_mm_ss").format(new java.util.Date())
+    df_audience_files.groupBy("request_id","uri").agg(sum(when($"cs_sha".isNull,0).otherwise(1)).alias("aud_count")).filter($"aud_count" === 0).
+      coalesce(1).write.csv(s"${inputHDFSPath}/outgoing/${sCurrDt}")
+    s"hdfs dfs -mv ${inputHDFSPath}/outgoing/${sCurrDt}/*.csv ${inputHDFSPath}/outgoing/cs_aud_data_miss_${sCurrDt}.csv".!
+    println(getDTFormat() + s": --> Audience Summary file: ${inputHDFSPath}/outgoing/cs_aud_summary_${sCurrDt}.csv")
+    s"hdfs dfs -rm -R $inputHDFSPath/outgoing/$sCurrDt".!
+
+    sCurrDt = new java.text.SimpleDateFormat("yyyy_MM_dd_HH_mm_ss").format(new java.util.Date())
+
+    df_audience_files.groupBy("request_id","uri").agg(sum(when($"cs_sha".isNull,0).otherwise(1)).alias("aud_count")).
+      coalesce(1).write.csv(s"${inputHDFSPath}/outgoing/${sCurrDt}")
+    s"hdfs dfs -mv ${inputHDFSPath}/outgoing/${sCurrDt}/*.csv ${inputHDFSPath}/outgoing/cs_aud_summary_${sCurrDt}.csv".!
+    println(getDTFormat() + s": --> Audience Summary file: ${inputHDFSPath}/outgoing/cs_aud_summary_${sCurrDt}.csv")
+    s"hdfs dfs -rm -R $inputHDFSPath/outgoing/$sCurrDt".!
+
+    //--- Create Aud summary file - END
 
     //------------Populate output data
     //Moved to beeline command
@@ -215,21 +221,21 @@ object json_csProcess01 {
     spark.sql(s"refresh table ${sDBName}.cs_request_audience")
     spark.sql(s"refresh table ${sDBName}.cs_audience")
     spark.sql(s"refresh table ${sDBName}.cs_response_data")
-//}catch{
-//  case ex: Exception => { println("---Exception while processing: " + ex) }
-  //case _: Exception => { println("---Exception while processing--") }
+    //}catch{
+    //  case ex: Exception => { println("---Exception while processing: " + ex) }
+    //case _: Exception => { println("---Exception while processing--") }
     //System.exit(999)
-//}
+    //}
     println(getDTFormat() + ": Completed. Exiting spark job.");
-}
+  }
 
 
-def getDT() : String = {
-  val dateFormatter = new SimpleDateFormat("yyyy_MM_dd_HH_mm_ss")
-  return dateFormatter.format(new Date())
-  //val today = Calendar.getInstance.getTime
-  //return dateFormatter.format(today)
-}
+  def getDT() : String = {
+    val dateFormatter = new SimpleDateFormat("yyyy_MM_dd_HH_mm_ss")
+    return dateFormatter.format(new Date())
+    //val today = Calendar.getInstance.getTime
+    //return dateFormatter.format(today)
+  }
   def getDTFormat(): String = {
     return new SimpleDateFormat("yyyy-MM-dd HH:mm:ss,SSS").format(new Date())
   }

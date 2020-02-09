ae6706190af2e13d4b33f586bb01057af3818d1f
kmbeyond <kiran.learn7@gmail.com>
2020-02-09 12:17:35 -0600

    Update SparkReadMultipleFiles.scala

--- src/main/scala/SparkReadMultipleFiles.scala
+++ src/main/scala/SparkReadMultipleFiles.scala
@@ -1,3 +1,6 @@
+import java.text.SimpleDateFormat
+import java.util.Date
+
 import org.apache.spark.sql.SparkSession
 
 /**
@@ -7,20 +10,22 @@ object SparkReadMultipleFiles {
 
   def main(args: Array[String]) {
 
-    val filePathSrc = "C:\\km\\as_AIA\\test_sample"
-            //"/home/kiran/km/km_hadoop/data/"
-    val filePathDest = "C:\\km\\as_AIA\\test_sample\\Spark_output_001"
-            //"/home/kiran/km/km_hadoop/data/fileslist"
+    //val filePathSrc = "C:\\km\\as_AIA\\test_sample"
+    val filePathSrc = "/home/kiran/km/km_big_data/data"
+
+    //val filePathDest = "C:\\km\\as_AIA\\test_sample\\Spark_output_001"
+    val filePathDest = "/home/kiran/km/km_big_data/data/fileslist"
+
     val spark = SparkSession
       .builder()
-      .config("spark.sql.warehouse.dir", "file:///c:/tmp/spark-warehouse")
+      //.config("spark.sql.warehouse.dir", "file:///c:/tmp/spark-warehouse")
       .master("local")
-      .appName("SQLContext Read CSV Demo")
+      .appName("Read Files")
       .getOrCreate
 
     spark.conf.set("spark.executor.memory", "2g")
     val sc = spark.sparkContext
-    sc.setLogLevel("INFO")
+    sc.setLogLevel("ERROR")
 
     val data = sc.wholeTextFiles(filePathSrc, 10)
 
@@ -29,19 +34,25 @@ object SparkReadMultipleFiles {
 
 
     //list of files & lines in each file
-    var files = data.map{ case (filename, cont) => filename+"["+cont.split("\n").size+"]"}.
+    var files = data.map{ case (filename, cont) => filename+"  [ Rows="+cont.split("\n").size+" ]"}.
       zipWithIndex().
       map(x => (x._2+1, x._1)).
       sortByKey()
 
     files.collect().foreach(println)
 
-    files.saveAsTextFile(filePathDest)
+    files.saveAsTextFile(filePathDest+"_"+getDT())
 
     //Get an RDD of all lines
     val lines = data.flatMap{ case (filename, cont) => cont.split("\n") }
     lines.count()
 
 
+  }
+  def getDT() : String = {
+    val dateFormatter = new SimpleDateFormat("yyyy_MM_dd_HH_mm_ss")
+    return dateFormatter.format(new Date())
+    //val today = Calendar.getInstance.getTime
+    //return dateFormatter.format(today)
   }
 }
